1b1969381c5d1ff1aa5ff884f3f14b2976204e2e
Anthony Labrador <anthonylabrador.cs@gmail.com>
2026-09-17 11:45:55 +0800

    PRPO Bug Fixes and Improvements FINAL

--- resources/js/Pages/PRPO/ProductsIndex.jsx
+++ resources/js/Pages/PRPO/ProductsIndex.jsx
@@ -464,7 +464,12 @@ export default function ProductsIndex({ auth, products = [], suppliers = [] }) {
                                         />
                                     </label>
 
-                                    <PrimaryButton onClick={() => openProductModal(null)}>+ Add Product</PrimaryButton>
+                                    <PrimaryButton onClick={() => openProductModal(null)} className="gap-1.5">
+                                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={2.5} stroke="currentColor" className="w-4 h-4">
+                                            <path strokeLinecap="round" strokeLinejoin="round" d="M12 4.5v15m7.5-7.5h-15" />
+                                        </svg>
+                                        Add Product
+                                    </PrimaryButton>
                                 </>
                             )}
 
